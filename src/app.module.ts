16b7a6a4a22db79e18699d29b2642a4d7f0cda06
Julia Chekan <juliabel5@gmail.com>
2024-11-09 19:16:02 +0300

    feat: add user endpont

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [
+    AppService,
+    UsersService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,73 @@
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from '../types/types';
+import { CreateUserDto } from './create-user.dto';
+import { UpdatePasswordDto } from './update-password.dto';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+
+  //constructor() {} // private readonly userRepository: UserRepository
+
+  async findAll(): Promise<User[]> {
+    // return await this.userRepository.find();
+    return this.users;
+  }
+
+  async findOneById(id: string): Promise<User | undefined> {
+    // return await this.userRepository.findOne(id);
+    return this.users.find((user) => user.id === id);
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser: User = {
+      id: uuidv4(),
+      login: createUserDto.login,
+      password: createUserDto.password,
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    // await this.userRepository.save(newUser);
+
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  async updatePassword(
+    id: string,
+    updatePasswordDto: UpdatePasswordDto,
+  ): Promise<User> {
+    const user = await this.findOneById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (user.password !== updatePasswordDto.oldPassword) {
+      throw new ForbiddenException('Old password is incorrect');
+    }
+
+    user.password = updatePasswordDto.newPassword;
+    user.version++;
+    user.updatedAt = Date.now();
+
+    // await this.userRepository.save(user);
+
+    return user;
+  }
+
+  async delete(id: string): Promise<void> {
+    const userIndex = await this.findOneById(id);
+    if (!userIndex) {
+      throw new NotFoundException('User not found');
+    }
+
+    // await this.userRepository.delete(id); // Если  ORM
+    this.users = this.users.filter((user) => user.id !== id);
+  }
+}
